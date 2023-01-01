a6fd168b09fd4cea20e910682428b1fbb8d0b107
Wyatt Baldwin <self@wyattbaldwin.com>
2023-01-01 10:14:19 -0800

    WIP

--- src/colors/routes/page.tsx
+++ src/colors/routes/page.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 
 import { useInterval } from "../hooks";
 
+const TWO_PI = Math.PI * 2;
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
@@ -31,122 +33,94 @@ const RunState = styled.div`
   }
 `;
 
+function getInitialRadius(w, h) {
+  const initialRadius = Math.floor(Math.min(w, h) / 4);
+  return initialRadius % 2 === 0 ? initialRadius : initialRadius - 1;
+}
+
 function randIn(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function draw(canvas, radius, color) {
+  const ctx = canvas.getContext("2d");
+
+  if (!ctx) {
+    return radius;
+  }
+
+  const [w, h] = [canvas.clientWidth, canvas.clientHeight];
+  const [cx, cy] = [Math.floor(w / 2), Math.floor(h / 2)];
+
+  ctx.fillStyle = color;
+  ctx.strokeStyle = color;
+  ctx.lineWidth = 2;
+
+  // Top left
+  ctx.beginPath();
+  ctx.arc(cx - radius, cy - radius, radius, 0, TWO_PI, false);
+  ctx.stroke();
+
+  // Top right
+  ctx.beginPath();
+  ctx.arc(cx + radius, cy - radius, radius, 0, TWO_PI, false);
+  ctx.stroke();
+
+  // Bottom right
+  ctx.beginPath();
+  ctx.arc(cx + radius, cy + radius, radius, 0, TWO_PI, false);
+  ctx.stroke();
+
+  // Bottom left
+  ctx.beginPath();
+  ctx.arc(cx - radius, cy + radius, radius, 0, TWO_PI, false);
+  ctx.stroke();
+
+  let newRadius = radius / 2;
+  if (newRadius < 8) {
+    newRadius = getInitialRadius(w, h) - randIn(4, Math.min(cx, cy) / 8);
+  }
+  return newRadius;
+}
+
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
   const [running, setRunning] = useState(false);
+  const [radius, setRadius] = useState(0);
   const [bgColor, setBgColor] = useState(0);
 
-  const r = 10;
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [reverseX, setReverseX] = useState(false);
-  const [reverseY, setReverseY] = useState(false);
-
+  const toggleRunning = () => setRunning(!running);
   const fgColor = (bgColor + 180) % 360;
   const backgroundColor = `hsl(${bgColor}, 100%, 50%)`;
   const color = `hsl(${fgColor}, 100%, 50%)`;
-  const toggleRunning = () => setRunning(!running);
 
-  // Mount
-  //
-  // 1. Set canvas width.
-  // 2. Set initial circle coordinates randomly in top left.
-  // 3. Start running.
+  // On mount, set canvas dimensions and start.
   useEffect(() => {
     const canvas = canvasRef?.current;
     if (canvas) {
-      const w = canvas.clientWidth;
-      const h = canvas.clientHeight;
-      const [minX, maxX] = [r, randIn(r, w / 8)];
-      const [minY, maxY] = [r, randIn(r, h / 8)];
-      canvas.width = w;
-      canvas.height = h;
-      setX(randIn(minX, maxX));
-      setY(randIn(minY, maxY));
+      const [w, h] = [canvas.clientWidth, canvas.clientHeight];
+      [canvas.width, canvas.height] = [w, h];
+      setRadius(getInitialRadius(w, h));
       setRunning(true);
     }
   }, []);
 
   // Update background color.
   useInterval(
-    useCallback(() => {
-      setBgColor((bgColor + 1) % 360);
-    }, [bgColor]),
-    100,
+    useCallback(() => setBgColor((bgColor + 1) % 360), [bgColor]),
+    200,
     canvasRef?.current && running,
     "update background color"
   );
 
   useInterval(
-    useCallback(() => {
-      const canvas = canvasRef?.current;
-      if (canvas) {
-        const w = canvas.clientWidth;
-        const h = canvas.clientHeight;
-
-        // Distance to move
-        let xm = Math.random();
-        let ym = Math.random();
-
-        // Weighting of whether to move in positive (right/down) or
-        // negative (left/up) direction. A larger number means greater
-        // probability of moving in the positive direction. The basic
-        // idea is to generally move right/down with a little gitter.
-        let xdp = 80 + randIn(-8, 8);
-        let ydp = 56 + randIn(-2, 2);
-
-        if (reverseX) {
-          xm = -xm;
-          ydp = xdp + randIn(-8, 8);
-        }
-        if (reverseY) {
-          ym = -ym;
-          xdp = ydp + randIn(-2, 2);
-        }
-
-        let newX = x + (randIn(0, 100) < xdp ? xm : -xm);
-        let newY = y + (randIn(0, 100) < ydp ? ym : -ym);
-
-        if (newX < r) {
-          newX = r;
-        } else if (newX > w - r) {
-          newX = w - r;
-        }
-
-        if (newY < r) {
-          newY = r;
-        } else if (newY > h - r) {
-          newY = h - r;
-        }
-
-        const ctx = canvas.getContext("2d");
-        if (ctx) {
-          ctx.fillStyle = color;
-          ctx.beginPath();
-          ctx.arc(x, y, r, 0, Math.PI * 2, false);
-          ctx.fill();
-        }
-        const lx = w - r;
-        const ly = h - r;
-        if (newX >= lx && !reverseX) {
-          setReverseX(true);
-        } else if (newX <= r && reverseX) {
-          setReverseX(false);
-        }
-        if (newY >= ly && !reverseY) {
-          setReverseY(true);
-        } else if (newY <= r && reverseY) {
-          setReverseY(false);
-        }
-        setX(newX);
-        setY(newY);
-      }
-    }, [color, x, y]),
-    1,
+    useCallback(
+      () =>
+        setRadius(draw(canvasRef?.current as HTMLCanvasElement, radius, color)),
+      [color, radius]
+    ),
+    50,
     canvasRef?.current && running,
     "update circle"
   );
